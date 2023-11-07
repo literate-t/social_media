f307c66bf15d3c515e88574454a53fd24419237f
literate <gaekoon@gmail.com>
2023-11-07 21:16:19 +0900

    Feat: 추가 posts 전체를 가져오는 GET API

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -11,7 +11,8 @@ import { PostsService } from './posts.service';
  * commentCount: number
  */
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -19,18 +20,53 @@ interface Post {
   commentCount: number;
 }
 
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'marvel',
+    title: 'ironman',
+    content: '아크 원자로 만들고 있는 로다쥬',
+    likeCount: 100000,
+    commentCount: 99999
+  },
+  {
+    id: 2,
+    author: 'marvel',
+    title: 'thor',
+    content: '망치의 신이 된 토르',
+    likeCount: 200000,
+    commentCount: 8888
+  },
+  {
+    id: 3,
+    author: 'marvel',
+    title: 'hulk',
+    content: '친절한 헐크씨',
+    likeCount: 300000,
+    commentCount: 77777
+  },
+];
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
-
+  
+  // 1) Get /posts
+  //    모든 포스트를 가져오기
   @Get()
-  getPost(): Post {
-    return {
-      author: 'marvel',
-      title: 'ironman',
-      content: '아크 원자로 개발한 아이언맨',
-      likeCount: 1000000,
-      commentCount: 9999
-    }
+  getPosts(): PostModel[] {
+    return posts;
   }
+  
+  // 2) GET /posts/:Id
+  //    id에 해당되는 post를 가져오기
+  
+  // 3) POST /posts
+  //    post 생성하기
+  
+  // 4) PUT /posts/:id
+  //    id에 해당되는 POST를 변경한다
+  
+  // 5) DELETE /posts/:id
+  //    id에 해당하는 POST를 삭제한다
 }
